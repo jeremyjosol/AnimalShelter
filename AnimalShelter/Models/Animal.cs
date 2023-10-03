77f56622abd677f64951a1c5b824e2c6995c1ea2
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-03 12:38:35 -0700

    All Views render + add additional properties

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -7,5 +7,9 @@ namespace AnimalShelter.Models
   {
     public int AnimalId { get; set; }
     public string Type { get; set; }
+    public string Breed { get; set; }
+    public string Name { get; set; }
+    public string AdmittanceDate { get; set; }
+
   }
 }
