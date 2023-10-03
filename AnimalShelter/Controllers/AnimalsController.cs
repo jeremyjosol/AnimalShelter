8e0291a1e2bd1d49fecc39f589efd51e006dd765
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-03 15:10:57 -0700

    Successfully delete data from Workbench database

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 
-// Research how to delete data from database
-
 namespace AnimalShelter.Controllers
 {
   public class AnimalsController : Controller
