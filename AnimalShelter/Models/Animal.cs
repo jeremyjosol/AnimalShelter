8822851ac48bd8f9d6071d7d2a9c459336b8f8a9
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-03 11:32:52 -0700

    Add boilerplate code

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using MySqlConnector;
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public int Type { get; set; }
+  }
+}
