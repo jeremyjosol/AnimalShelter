47b226ec66f507107c6dedab439b956250366d73
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-03 12:27:49 -0700

    Add Details method

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -30,5 +30,10 @@ namespace AnimalShelter.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+    {
+    Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+    return View(thisAnimal);
+    }
   }
 }
